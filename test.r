d60182f1a7a0edfcad491146a61995c9576be4e9
lakik <lakik@gmx.de>
2014-05-22 11:47:21 +0200

    test

--- test.r
+++ test.r
@@ -1,4 +1,4 @@
-bdyad=paste(zdata$focal, zdata$other, sep="_") 
-bdyad=unlist(lapply(as.character(bdyad), function(x){
-  paste(sort(unlist(strsplit(x, split="_"))), collapse="_") 
-}))
+add=function(x){
+    x+x
+}
+
